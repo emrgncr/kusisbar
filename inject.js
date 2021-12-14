a2a55469bbb140e2e4573d59e3142c7550321bb4
emrgncr <egencer20@ku.edu.tr>
2021-12-14 17:09:53 +0300

    added other tabs, changed style

--- inject.js
+++ inject.js
@@ -2,10 +2,13 @@
 console.log("asdasd")
 
 const buttons = {
+    "Student Center":["Student Center"],
     "Class Search":["Class Search"],
     "Enrollment":["View My Assignments","Enrollment Dates","My Weekly Schedule","View My Exam Schedule","View My Class Attendance"],
-    "Campus Finances":["Account Inquiry"],
-    "Student Center":["Student Center"],
+    "Program Enrollment":["My Progression Report","My Course Planner","My Planned Enrollments","My Confirmed Enrollments"],
+    "Academic Records":["My Course History","My Advisors"],
+    "Work&Study":["My Profile","Application Status","Work History"],
+    "Dormitory":["Requests"],
 }
 const links = {
     "Class Search":"https://kusis.ku.edu.tr/psp/ps/EMPLOYEE/SA/c/SA_LEARNER_SERVICES.CLASS_SEARCH.GBL?FolderPath=PORTAL_ROOT_OBJECT.CO_EMPLOYEE_SELF_SERVICE.HCCC_SS_CATALOG.HC_CLASS_SEARCH&IsFolder=false&IgnoreParamTempl=FolderPath%2cIsFolder",
@@ -16,7 +19,16 @@ const links = {
     "View My Class Attendance":"https://kusis.ku.edu.tr/psp/ps/EMPLOYEE/SA/c/KU_CA_MENU.KU_CA_SSS_STATS.GBL?FolderPath=PORTAL_ROOT_OBJECT.CO_EMPLOYEE_SELF_SERVICE.HCCC_ENROLLMENT.KU_CA_SSS_STATS_GBL&IsFolder=false&IgnoreParamTempl=FolderPath%2cIsFolder",
     "Account Inquiry":"https://kusis.ku.edu.tr/psp/ps/EMPLOYEE/SA/c/SA_LEARNER_SERVICES.SSF_SS_ACCT_SUMM.GBL?FolderPath=PORTAL_ROOT_OBJECT.CO_EMPLOYEE_SELF_SERVICE.HCCC_FINANCES.HC_SF_ACCT_SUMM_SS_GBL1&IsFolder=false&IgnoreParamTempl=FolderPath%2cIsFolder",
     "Student Center":"https://kusis.ku.edu.tr/psp/ps/EMPLOYEE/SA/c/SA_LEARNER_SERVICES.SSS_STUDENT_CENTER.GBL?FolderPath=PORTAL_ROOT_OBJECT.CO_EMPLOYEE_SELF_SERVICE.HC_SSS_STUDENT_CENTER&IsFolder=false&IgnoreParamTempl=FolderPath%2cIsFolder",
-
+    "My Course History":"https://kusis.ku.edu.tr/psp/ps/EMPLOYEE/SA/c/SA_LEARNER_SERVICES.SSS_MY_CRSEHIST.GBL?FolderPath=PORTAL_ROOT_OBJECT.CO_EMPLOYEE_SELF_SERVICE.HCCC_ACADEMIC_RECORDS.HC_SSS_MY_CRSEHIST_GBL&IsFolder=false&IgnoreParamTempl=FolderPath%2cIsFolder",
+    "My Advisors":"https://kusis.ku.edu.tr/psp/ps/EMPLOYEE/SA/c/SA_LEARNER_SERVICES.SSR_SSADVR.GBL?FolderPath=PORTAL_ROOT_OBJECT.CO_EMPLOYEE_SELF_SERVICE.HCCC_ACADEMIC_RECORDS.HC_SSR_SSADVR_GBL&IsFolder=false&IgnoreParamTempl=FolderPath%2cIsFolder",
+    "My Progression Report":"https://kusis.ku.edu.tr/psp/ps/EMPLOYEE/SA/c/SSR_PROG_ENRL_SS.SSR_PGTR_DE_CMP.GBL?FolderPath=PORTAL_ROOT_OBJECT.CO_EMPLOYEE_SELF_SERVICE.HCCC_PE_STUDENT.HC_SSR_PGTR_DE_CMP&IsFolder=false&IgnoreParamTempl=FolderPath%2cIsFolder",
+    "My Course Planner":"https://kusis.ku.edu.tr/psp/ps/EMPLOYEE/SA/c/SSR_PROG_ENRL_SS.SSS_PE_SS_PLNR.GBL?FolderPath=PORTAL_ROOT_OBJECT.CO_EMPLOYEE_SELF_SERVICE.HCCC_PE_STUDENT.HC_SSS_PE_SS_PLNR_GBL&IsFolder=false&IgnoreParamTempl=FolderPath%2cIsFolder",
+    "My Planned Enrollments":"https://kusis.ku.edu.tr/psp/ps/EMPLOYEE/SA/c/SSR_PROG_ENRL_SS.SSR_APT_SCHD_BLDR.GBL?FolderPath=PORTAL_ROOT_OBJECT.CO_EMPLOYEE_SELF_SERVICE.HCCC_PE_STUDENT.HC_SSR_APT_SCHD_BLDR_GBL&IsFolder=false&IgnoreParamTempl=FolderPath%2cIsFolder",
+    "My Confirmed Enrollments":"https://kusis.ku.edu.tr/psp/ps/EMPLOYEE/SA/c/SSR_PROG_ENRL_SS.SSR_SS_MY_CLASSES.GBL?FolderPath=PORTAL_ROOT_OBJECT.CO_EMPLOYEE_SELF_SERVICE.HCCC_PE_STUDENT.HC_SSR_SS_MY_CLASSES&IsFolder=false&IgnoreParamTempl=FolderPath%2cIsFolder",
+    "My Profile":"https://kusis.ku.edu.tr/psp/ps/EMPLOYEE/SA/c/KU_WS_MNG.KU_WS_SS_PROFILE.GBL?FolderPath=PORTAL_ROOT_OBJECT.CO_EMPLOYEE_SELF_SERVICE.KU_WORK_STUDY.KU_WS_SS_PROFILE&IsFolder=false&IgnoreParamTempl=FolderPath%2cIsFolder",
+    "Application Status":"https://kusis.ku.edu.tr/psp/ps/EMPLOYEE/SA/c/KU_WS_MNG.KU_WS_SS_A_STATUS.GBL?FolderPath=PORTAL_ROOT_OBJECT.CO_EMPLOYEE_SELF_SERVICE.KU_WORK_STUDY.KU_WS_SS_A_STATUS&IsFolder=false&IgnoreParamTempl=FolderPath%2cIsFolder",
+    "Work History":"https://kusis.ku.edu.tr/psp/ps/EMPLOYEE/SA/c/KU_WS_MNG.KU_WS_SS_WRK_HRS.GBL?FolderPath=PORTAL_ROOT_OBJECT.CO_EMPLOYEE_SELF_SERVICE.KU_WORK_STUDY.KU_WS_SS_WRK_HRS&IsFolder=false&IgnoreParamTempl=FolderPath%2cIsFolder",
+    "Requests":"https://kusis.ku.edu.tr/psp/ps/EMPLOYEE/SA/c/SSR_RS_SELFSERV_MGMT.SSS_RS_STDNT_REQ.GBL?FolderPath=PORTAL_ROOT_OBJECT.CO_EMPLOYEE_SELF_SERVICE.HCSR_REQUEST_MGMT.HC_SSS_RS_STDNT_REQ_GBL&IsFolder=false&IgnoreParamTempl=FolderPath%2cIsFolder",
 }
 
 let logo = document.getElementById("kuhdrlogo")
@@ -25,8 +37,18 @@ logo.addEventListener("click",()=>{
 })
 logo.style.cursor = "pointer";
 
+let x = Object.keys(buttons).length;
+x = 85/x;
 
-const button_width = "220px";
+const button_width = `${x}%`;
+
+const button_color = "whitesmoke"
+const button_hover_color = "rgb(238, 238, 238)"
+const button_text_color = "rgba(171,91,26,1)"
+const button_hover_text_color = "rgba(171,91,26,1)"
+
+
+
 
 let parent = document.body.children.item(1).firstElementChild
 let bar = document.createElement("div")
@@ -34,7 +56,7 @@ let bar = document.createElement("div")
 
 
 //make bar
-bar.style.height = "30px";
+bar.style.height = "36px";
 bar.style.backgroundColor = "snow";
 bar.style.borderRadius = "4px";
 bar.style.display = "flex";
@@ -48,18 +70,33 @@ buttonM.style.display = "flex";
 buttonM.style.width = button_width;
 buttonM.style.height = "fit-content";
 buttonM.style.alignContent = "center";
+buttonM.style.justifyContent = "center";
 buttonM.style.flexDirection = "column";
+// buttonM.style.paddingLeft = "2px";
+// buttonM.style.paddingRight = "2px";
+// buttonM.style.backgroundColor =  "red";
+buttonM.style.overflow ="hidden";
 let button = document.createElement("div")
-button.style.width = "100%";
+button.style.width = "95%";
+button.style.display = "flex";
 button.style.flexDirection = "column";
-button.style.paddingLeft = "10px";
-button.style.paddingRight = "10px";
+button.style.borderWidth = "2px";
+button.style.borderColor = "black";
+button.style.borderStyle = "solid";
+// button.style.paddingLeft = "10px";
+// button.style.paddingRight = "10px";
 button.innerText = i;
-button.style.backgroundColor = "blue"
-button.style.color = "snow";
-button.style.borderRadius = "3px";
-button.style.height = "26px";
+button.style.backgroundColor = button_color
+button.style.color = button_text_color
+button.style.borderRadius = "2px";
+button.style.height = "fit-content";
+button.style.minHeight = "32px";
+button.style.zIndex = "99"
 button.style.marginTop = "2px";
+button.style.alignContent = "center";
+button.style.justifyContent = "center";
+button.style.alignSelf = "center";
+button.style.justifySelf = "center";
 // button.style.marginBottom = "2px";
 button.style.textAlign = "center";
 buttonM.appendChild(button)
@@ -67,16 +104,23 @@ buttonM.addEventListener("mouseenter",()=>{
 let ls = buttons[buttonM.innerText];
 for(j in buttons[buttonM.innerText]){
     let sbut = document.createElement("div")
-    sbut.style.width = "100%";
-    sbut.style.color = "snow";
-    sbut.style.paddingLeft = "10px";
-    sbut.style.paddingRight = "10px";
+    sbut.style.width = "95%";
+    sbut.style.color = button_text_color
+    // sbut.style.paddingLeft = "10px";
+    // sbut.style.paddingRight = "10px";
+
+    sbut.style.borderWidth = "2px";
+    sbut.style.borderColor = "black";
+    sbut.style.borderStyle = "solid";
+
     sbut.style.paddingTop = "3px";
     sbut.style.paddingBottom = "3px";
+    sbut.style.alignSelf = "center";
+    sbut.style.justifySelf = "center";
     console.log(ls,j)
     sbut.innerText = ls[j];
-    sbut.style.backgroundColor = "blue"
-    sbut.style.borderRadius = "3px";
+    sbut.style.backgroundColor = button_color
+    sbut.style.borderRadius = "2px";
     sbut.style.height = "80%";
     sbut.style.marginTop = "0px";
     sbut.style.marginBottom = "0px";
@@ -86,6 +130,17 @@ for(j in buttons[buttonM.innerText]){
     sbut.style.cursor = "pointer";
     sbut.className =`sil_${button.innerText}`;
     sbut.id = "sil";
+
+    sbut.addEventListener("mouseenter",()=>{
+        sbut.style.backgroundColor = button_hover_color;
+        sbut.style.color = button_hover_text_color;
+    })
+    sbut.addEventListener("mouseleave",()=>{
+        sbut.style.backgroundColor = button_color;
+        sbut.style.color = button_text_color;
+    })
+
+
     let abut = document.createElement("div")
     abut.style.height = "3px";
     abut.style.width = "100%";
